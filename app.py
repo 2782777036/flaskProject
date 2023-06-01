c7b0690abb4417ab86301f6d8014bb4ba76c687e
ke.ma <ke.ma@mei1.com>
2023-06-01 16:56:45 +0800

    Initial commit

--- app.py
+++ app.py
@@ -63,4 +63,4 @@ def get_messages():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-        socketio.run(app)
+        socketio.run(app.run(host='0.0.0.0', port=2333))
